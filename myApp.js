0f7e31d8cbf3f17e81ae553c802271c4177a0e9e
arah44 <aranjoyce1@gmail.com>
2022-06-19 13:32:03 +0300

    updated middleware message to match challenge

--- myApp.js
+++ myApp.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
 let app = express();
 
 app.use(((req, res, next) => {
-  console.log(req.method, req.path + '-' + req.ip)
+  console.log(req.method, req.path + " - " + req.ip)
   next();
 }))
 
-app.use('/public', express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"))
 
-app.get('/', (req, res) => (
+app.get("/", (req, res) => (
   res.sendFile(__dirname + "/views/index.html")
 ))
 
