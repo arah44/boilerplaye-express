29fe333bd7d02b0cadc7053507271c43c217974c
arah44 <aranjoyce1@gmail.com>
2022-06-19 12:34:56 +0300

    Added /json endpoint

--- myApp.js
+++ myApp.js
@@ -3,9 +3,13 @@ let app = express();
 
 app.use('/public', express.static(__dirname + "/public"))
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => (
   res.sendFile(__dirname + "/views/index.html")
-})
+))
+
+app.get("/json", (req, res) => (
+  res.json({"message": "Hello json"})
+))
 
 
 
